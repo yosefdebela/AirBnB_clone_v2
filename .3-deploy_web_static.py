9f471716de69a6ffd28e60f89522c9c962cfe29a
yos <yosef.debela@ju.edu.et>
2024-01-03 00:58:07 +0300

    full deploy

--- .3-deploy_web_static.py
+++ .3-deploy_web_static.py
@@ -12,6 +12,7 @@ env.user = 'ubuntu'
 
 env.key_filename = '~/.ssh/school'
 
+
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
@@ -46,13 +47,13 @@ def do_deploy(archive_path):
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+                   format(name)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+                   format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+                   format(file, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
@@ -60,12 +61,12 @@ def do_deploy(archive_path):
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+                   format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+                   format(name)).failed is True:
         return False
     return True
 
