4f904a1d3c4c2a751b4639a1120e62f49ba7e6b9
yos <yosef.debela@ju.edu.et>
2024-06-09 21:47:41 +0300

    full

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -33,7 +33,7 @@ def do_pack():
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {}.tgz web_static".format(file)).failed is True:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
 
