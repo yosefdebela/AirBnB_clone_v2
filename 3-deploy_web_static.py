53dce48108991b92c984959b097b02de01238eba
yos <yosef.debela@ju.edu.et>
2024-06-09 21:42:59 +0300

    full

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os.path
 import os
-import dotenv
+# import dotenv
 from dotenv import load_dotenv
 from datetime import datetime
 from fabric.api import env
@@ -33,7 +33,7 @@ def do_pack():
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if local("tar -cvzf {}.tgz web_static".format(file)).failed is True:
         return None
     return file
 
