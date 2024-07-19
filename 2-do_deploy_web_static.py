9dfe5f1f5457947692e9830c27bf748f880c043e
yos <yosef.debela@ju.edu.et>
2024-07-19 04:24:38 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -47,7 +47,7 @@ releases/web_static_{}/'.format(timestamp))
         run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
         # move contents into host web_static
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+        run('sudo mv  /data/web_static/releases/web_static_{}/web_static/* \
 /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
         # remove extraneous web_static dir
