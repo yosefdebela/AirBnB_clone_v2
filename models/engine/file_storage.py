1b2f3f52cafe7236fa27eae675e9f73354905bab
yosefdebela <yosef.debela@ju.edu.et>
2025-05-19 07:47:08 +0300

    delete storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,6 +67,13 @@ class FileStorage:
                         self.new(cls(**o))
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it exists"""
+        if obj is None:
+            return
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        if key in self.__objects:
+            del self.__objects[key]
 
     def close(self):
         """Call the reload method."""
