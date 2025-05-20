7016ba59cd9addab44637e7aa26b0fba6de0934f
yosefdebela <yosef.debela@ju.edu.et>
2025-05-20 05:00:40 +0300

    sql

--- models/.ipynb_checkpoints/__init__-checkpoint.py
+++ models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -8,7 +8,7 @@ from os import getenv
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage_tempo import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
